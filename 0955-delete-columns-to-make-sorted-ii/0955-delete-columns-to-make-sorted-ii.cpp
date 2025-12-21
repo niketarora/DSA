af3b5422f3f5b400939da3426667e14945d855d2
niketarora <niketarora682@gmail.com>
2025-12-21 20:30:04 +0530

    Time: 1 ms (28.09%), Space: 12.9 MB (62.92%) - LeetHub

---
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int rows = strs.size();
+        int cols = strs[0].size();
+        int deletion = 0;
+       vector<bool> alreadySorted(rows, false);
+        int row = 0;
+        int col = 0;
+        for (col = 0; col < cols; col++) {
+            bool deleted = false;
+            for (row = 0; row < rows - 1; row++) {
+                if (!alreadySorted[row] &&
+                    strs[row][col] > strs[row + 1][col]) {
+
+                    deletion++;
+                    deleted = true;
+                    break;
+                }
+            }
+            if (deleted) {
+                continue;
+            }
+            for (int i = 0; i < rows - 1; i++) {
+                alreadySorted[i] =
+                    alreadySorted[i] ||
+                    (strs[i][col] < strs[i + 1][col]);
+            }
+        }
+        return deletion;
+
+    }
+};
